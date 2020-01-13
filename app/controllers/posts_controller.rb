30a65a291e4614ea5c6d46253fcdf6333abfa120
github username <github email address>
2020-01-13 03:00:54 -0800

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,12 @@ class PostsController < ApplicationController
   end
 
   # add create method here
+  def create
+    @post = Post.new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
+    # redirected to the show page for the post user just created.
+    redirect_to post_path(@post)
+  end
 end
